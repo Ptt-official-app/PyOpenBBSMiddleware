beab636de1c40ee21065bba5fe0795dd7962b5d0
Chuan-Heng Hsiao <hsiao.chuanheng@gmail.com>
2020-12-03 01:29:16 -0500

    update version to 0.1.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="openbbs-middleware",
-    version='0.1.1',
+    version='0.1.2',
     author="",
     author_email="",
     description="",
